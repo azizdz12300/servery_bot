39cc849f9eb379106b2ef997793709ecb14167a7
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-03-31 15:02:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2987,7 +2987,7 @@ const channel = sWlc[message.guild.id].channel
 
 client.on("guildMemberAdd", member => {
       if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
-    channel: "welcome"
+    channel: "👑ترحيب👑"
   }
   const channel = sWlc[member.guild.id].channel
     const sChannel = sWlc[member.guild.id].channel
